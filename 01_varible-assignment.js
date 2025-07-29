92bf9e6bc47884b89a4c57bbd1d6f6dc220a5517
GajananW0711 <gajananwadje1999@gmail.com>
2025-07-29 16:30:43 +0530

    Create 01_varible-assignment.js

---
+++ 01_varible-assignment.js
@@ -0,0 +1,43 @@
+// var city ="Pune";
+// var bf= "Gajanan";
+// var country = "India";
+// var school ="Spicer";
+// var clg = "JSPM RSCOE"
+
+// console.log("My city name is " + city);
+// console.log("My Best Friend name is " + bf);
+// console.log( country + " is my country" );
+// console.log("My School name is " + school);
+// console.log("My College name is " + clg);
+
+
+
+var fullname;
+console.log("My full name is " + fullname);
+
+
+var fullname = "Gajanan";
+console.log("My full name is " + fullname);
+
+var fullname = "Gajanan Wadje";
+console.log("My full name is " + fullname);
+
+console.log("-----------------------------------------------------------");
+
+var love = "JavaScript";
+var firstname = "Gajanan";
+var lastname = "Wadje";
+var age = "25";
+var pincode = "411027";
+var gender = "Male";
+var married = "Single";
+
+
+console.log("I love only " + love);
+console.log("My First name is " + firstname);
+console.log("My Last name is " + lastname);
+console.log("My age is " + age);
+console.log("Pincode is " + pincode);
+console.log("My Gender" + gender);
+console.log("Married " + married);
+
