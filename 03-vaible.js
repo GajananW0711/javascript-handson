be8507153eecdfb1679626a18a62506a6420d0f1
GajananW0711 <gajananwadje1999@gmail.com>
2025-08-12 15:26:40 +0530

    update

---
+++ 03-vaible.js
@@ -0,0 +1,35 @@
+var name;
+console.log("my full name is : " +name);
+
+
+name="Gajanan Wadje";
+console.log("my full name is : " +name);
+
+
+name="Gajanan Balaji Wadje"
+console.log("my full name is : " +name);
+
+
+console.log("===================================================");
+
+
+myLove="I love only JS";
+console.log("I love only : " +myLove);
+
+firstName="Gajanan";
+console.log("my first name is : " +firstName);
+
+lastName="Wadje"
+console.log("my last name is : " +lastName);
+
+var number=25;
+console.log("my age is : " +number);
+
+number=411027;
+console.log("pincode is : " +number);
+
+var gender="Male";
+console.log("Gender : " +gender);
+
+var isMarried="Single";
+console.log("Married Status : " +isMarried);
