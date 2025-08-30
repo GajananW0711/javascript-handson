79873a2217ee4732163d1c03d73e6f6ff0009e2f
GajananW0711 <gajananwadje1999@gmail.com>
2025-08-30 08:02:17 +0530

    Create 09-set.js

---
+++ 09-set.js
@@ -0,0 +1,10 @@
+const rollNumber = new Set();
+
+rollNumber.add(11); 
+rollNumber.add(20); 
+rollNumber.add(30); 
+rollNumber.add(40); 
+rollNumber.add(40); 
+
+console.log`Give size:${rollNumber.size}`;
+console.log`Give size:${rollNumber.size}`;
